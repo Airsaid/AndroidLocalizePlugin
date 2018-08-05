23a672c50414683d4861de733aa790fd05af69eb
airsaid <airsaid1024@gmail.com>
2018-08-05 21:15:57 +0800

    Initial commit

---
+++ src/action/ConvertAction.java
@@ -0,0 +1,15 @@
+package action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author airsaid
+ */
+public class ConvertAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+
+    }
+}
