4b94aa9598c5e5504af76e82dfadaaa40ab440ef
airsaid <airsaid1024@gmail.com>
2018-08-08 00:02:38 +0800

    Added dialog file

--- src/action/ConvertAction.java
+++ src/action/ConvertAction.java
@@ -2,6 +2,9 @@ package action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.refactoring.ui.InfoDialog;
 
 /**
  * @author airsaid
@@ -10,6 +13,8 @@ public class ConvertAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-
+        Project project = e.getData(CommonDataKeys.PROJECT);
+        InfoDialog dialog = new InfoDialog("Info", project);
+        dialog.show();
     }
 }
