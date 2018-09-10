83fa72d4aacba8d083057ce4cf7d7f6f8dd9c8b1
Airsaid <airsaid1024@gmail.com>
2018-09-10 20:09:25 +0800

    Added google translation api

--- src/logic/TranslationTask.java
+++ src/logic/TranslationTask.java
@@ -8,6 +8,7 @@ import module.AndroidString;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.trans.GoogleApi;
 
 import java.util.List;
 
@@ -17,16 +18,27 @@ import java.util.List;
 public class TranslationTask extends Task.Backgroundable {
 
     private List<SupportLanguage> mLanguages;
-    private List<AndroidString> mAndroidStrings;
+    private List<AndroidString>   mAndroidStrings;
+    private final GoogleApi       mGoogleApi;
 
-    public TranslationTask(@Nullable Project project, @Nls @NotNull String title, List<SupportLanguage> languages, List<AndroidString> androidStrings) {
+    public TranslationTask(@Nullable Project project, @Nls @NotNull String title,
+                           List<SupportLanguage> languages, List<AndroidString> androidStrings) {
         super(project, title);
         this.mLanguages = languages;
         this.mAndroidStrings = androidStrings;
+        mGoogleApi = new GoogleApi();
     }
 
     @Override
     public void run(@NotNull ProgressIndicator progressIndicator) {
-
+        for (SupportLanguage language : mLanguages) {
+            System.out.println("翻译国家:" + language.getEnglishName());
+            for (AndroidString androidString : mAndroidStrings) {
+                String value = androidString.getValue();
+                String result = mGoogleApi.translate(value, "en", language.getCode());
+                System.out.println("翻译前:" + value);
+                System.out.println("翻译后:" + result);
+            }
+        }
     }
 }
