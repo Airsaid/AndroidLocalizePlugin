1af5e60be9ec535a80ab8e9c1a81a6c4eaf7b88e
Airsaid <airsaid1024@gmail.com>
2018-09-21 20:09:43 +0800

    Added google translate

--- src/logic/TranslationTask.java
+++ src/logic/TranslationTask.java
@@ -8,7 +8,6 @@ import module.AndroidString;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.trans.GoogleApi;
 
 import java.util.List;
 
@@ -19,26 +18,19 @@ public class TranslationTask extends Task.Backgroundable {
 
     private List<SupportLanguage> mLanguages;
     private List<AndroidString>   mAndroidStrings;
-    private final GoogleApi       mGoogleApi;
 
     public TranslationTask(@Nullable Project project, @Nls @NotNull String title,
                            List<SupportLanguage> languages, List<AndroidString> androidStrings) {
         super(project, title);
         this.mLanguages = languages;
         this.mAndroidStrings = androidStrings;
-        mGoogleApi = new GoogleApi();
     }
 
     @Override
     public void run(@NotNull ProgressIndicator progressIndicator) {
         for (SupportLanguage language : mLanguages) {
             System.out.println("翻译国家:" + language.getEnglishName());
-            for (AndroidString androidString : mAndroidStrings) {
-                String value = androidString.getValue();
-                String result = mGoogleApi.translate(value, "en", language.getCode());
-                System.out.println("翻译前:" + value);
-                System.out.println("翻译后:" + result);
-            }
+
         }
     }
 }
