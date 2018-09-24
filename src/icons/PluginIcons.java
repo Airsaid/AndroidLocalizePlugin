e92f690325cf24c453e42e8c07e4c5bbaf4cea86
airsaid <airsaid1024@gmail.com>
2018-09-24 14:13:22 +0800

    Added icon

---
+++ src/icons/PluginIcons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface PluginIcons {
+  Icon CONVERT_ACTION_ICON = IconLoader.getIcon("/icons/icon.png");
+}
