d98da42e028ab96ac0bfcde88b2c136a545e683c
Airsaid <airsaid1024@gmail.com>
2021-06-01 16:27:23 +0800

    Add comments to the Translator

--- src/main/java/com/airsaid/localization/translate/Translator.java
+++ src/main/java/com/airsaid/localization/translate/Translator.java
@@ -5,11 +5,24 @@ import com.airsaid.localization.translate.lang.Lang;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * The translator interface, the direct implementation class is {@link AbstractTranslator},
+ * and all translators should extends {@link AbstractTranslator} to avoid writing duplicate code.
+ *
  * @author airsaid
+ * @see AbstractTranslator
  * @see GoogleTranslator
  */
 public interface Translator {
 
+  /**
+   * Invoke translation operation.
+   *
+   * @param fromLang the language of text.
+   * @param toLang   the language to be translated into.
+   * @param text     the text to be translated.
+   * @return the translated text.
+   * @throws TranslationException this exception is thrown if the translation failed.
+   */
   String doTranslate(@NotNull Lang fromLang, @NotNull Lang toLang, @NotNull String text) throws TranslationException;
 
 }
