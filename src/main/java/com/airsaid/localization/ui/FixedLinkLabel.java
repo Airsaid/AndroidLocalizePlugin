953f956061c34fa2ab5a6d4b13d3ffaeefd3704d
Airsaid <airsaid1024@gmail.com>
2021-06-03 21:45:08 +0800

    Fix apply link response twice

--- src/main/java/com/airsaid/localization/ui/FixedLinkLabel.java
+++ src/main/java/com/airsaid/localization/ui/FixedLinkLabel.java
@@ -2,14 +2,19 @@ package com.airsaid.localization.ui;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.ui.components.labels.LinkLabel;
+import jdk.javadoc.internal.doclets.toolkit.util.DocLink;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 /**
+ * Fixed the problem that sometimes click does not respond.
+ *
  * @author airsaid
  */
 public class FixedLinkLabel extends LinkLabel<Object> {
+  private boolean isDoClick = false;
+
   public FixedLinkLabel() {
     super("", AllIcons.Ide.Link);
     addMouseListener(new MouseAdapter() {
@@ -19,6 +24,19 @@ public class FixedLinkLabel extends LinkLabel<Object> {
           doClick();
         }
       }
+
+      @Override
+      public void mouseExited(MouseEvent e) {
+        isDoClick = false;
+      }
     });
+  }
+
+  @Override
+  public void doClick() {
+    if (!isDoClick) {
+      isDoClick = true;
+      super.doClick();
+    }
   }
 }
