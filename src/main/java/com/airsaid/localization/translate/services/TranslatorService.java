a2e5fbf865edcd65e2d0d545312efb44d58f626c
Airsaid <airsaid1024@gmail.com>
2021-06-05 19:43:30 +0800

    Update translator order

--- src/main/java/com/airsaid/localization/translate/services/TranslatorService.java
+++ src/main/java/com/airsaid/localization/translate/services/TranslatorService.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2021 Airsaid. https://github.com/airsaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.airsaid.localization.translate.services;
 
 import com.airsaid.localization.translate.AbstractTranslator;
@@ -13,7 +30,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -32,7 +49,7 @@ public final class TranslatorService {
   private boolean isEnableCache = true;
 
   public TranslatorService() {
-    translators = new HashMap<>();
+    translators = new LinkedHashMap<>();
 
     GoogleTranslator googleTranslator = new GoogleTranslator();
     translators.put(googleTranslator.getKey(), googleTranslator);
