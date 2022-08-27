ae751d44fef021092c919e64e37bae7b3df6b7bb
Airsaid <airsaid1024@gmail.com>
2022-08-27 11:30:17 +0800

    Add signature configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,12 @@ tasks {
     useJUnitPlatform()
   }
 
+  signPlugin {
+    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+    privateKey.set(System.getenv("PRIVATE_KEY"))
+    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  }
+
   publishPlugin {
     token.set(System.getenv("PUBLISH_TOKEN"))
   }
