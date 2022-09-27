b8dc4a62f8b888fc150d0f5178d6d3f02fa06d14
Airsaid <airsaid1024@gmail.com>
2022-09-27 20:54:12 +0800

    Squashed commit of the following:
    commit 23ea8326a93a65f10644ac2ed4000a1d67d5dcd7
    Merge: 5470884 54cc5a0
    Author: Yoo Zhou <airsaid1024@gmail.com>
    Date:   Tue Sep 27 20:20:46 2022 +0800
    
        Merge branch 'master' into dependabot/gradle/org.junit.jupiter-junit-jupiter-api-5.9.1
    
    commit 5470884fc0a090eee1386de170c673864cd6807b
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Mon Sep 26 14:38:37 2022 +0000
    
        Bump junit-jupiter-api from 5.9.0 to 5.9.1
    
        Bumps [junit-jupiter-api](https://github.com/junit-team/junit5) from 5.9.0 to 5.9.1.
        - [Release notes](https://github.com/junit-team/junit5/releases)
        - [Commits](https://github.com/junit-team/junit5/compare/r5.9.0...r5.9.1)
    
        ---
        updated-dependencies:
        - dependency-name: org.junit.jupiter:junit-jupiter-api
          dependency-type: direct:production
          update-type: version-update:semver-patch
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -106,6 +106,6 @@ dependencies {
   implementation("com.google.code.gson:gson:2.9.1")
   implementation("com.aliyun:alimt20181012:1.0.3")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
   testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
