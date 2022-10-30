1039f9a1bef4fe0bbcd22c8c1a60d1b4303c77bc
Musagil Musabayli <musaqil@gmail.com>
2022-10-30 14:12:50 +0100

    Add DeepL Support

---
+++ src/main/java/com/airsaid/localization/translate/impl/deepl/DeepLTranslationResult.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright 2021 Airsaid. https://github.com/airsaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.airsaid.localization.translate.impl.deepl;
+
+import com.airsaid.localization.translate.TranslationResult;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author musagil
+ */
+public class DeepLTranslationResult implements TranslationResult {
+
+  private List<Translation> translations;
+
+  @Override
+  public @NotNull String getTranslationResult() {
+    if (translations != null && !translations.isEmpty()) {
+      String result = translations.get(0).getText();
+      return result != null ? result : "";
+    }
+    return "";
+  }
+
+  public List<Translation> getTranslations() {
+    return translations;
+  }
+
+  public void setTranslations(List<Translation> translations) {
+    this.translations = translations;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    DeepLTranslationResult that = (DeepLTranslationResult) o;
+    return Objects.equals(translations, that.translations);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(translations);
+  }
+
+  @Override
+  public String toString() {
+    return "DeepLTranslationResult{" +
+        "translations=" + translations +
+        '}';
+  }
+
+  public static class Translation {
+    private String text;
+    private String to;
+
+    public String getText() {
+      return text;
+    }
+
+    public void setText(String text) {
+      this.text = text;
+    }
+
+    public String getTo() {
+      return to;
+    }
+
+    public void setTo(String to) {
+      this.to = to;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Translation that = (Translation) o;
+      return Objects.equals(text, that.text) && Objects.equals(to, that.to);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(text, to);
+    }
+
+    @Override
+    public String toString() {
+      return "Translation{" +
+          "text='" + text + '\'' +
+          ", to='" + to + '\'' +
+          '}';
+    }
+  }
+
+  public static class Error {
+    private String code;
+    private String message;
+
+    public String getCode() {
+      return code;
+    }
+
+    public void setCode(String code) {
+      this.code = code;
+    }
+
+    public String getMessage() {
+      return message;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Error error = (Error) o;
+      return Objects.equals(code, error.code) && Objects.equals(message, error.message);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(code, message);
+    }
+
+    @Override
+    public String toString() {
+      return "Error{" +
+          "code='" + code + '\'' +
+          ", message='" + message + '\'' +
+          '}';
+    }
+  }
+}
---
+++ src/main/java/com/airsaid/localization/translate/impl/deepl/DeepLTranslator.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2021 Airsaid. https://github.com/airsaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.airsaid.localization.translate.impl.deepl;
+
+import com.airsaid.localization.translate.AbstractTranslator;
+import com.airsaid.localization.translate.lang.Lang;
+import com.airsaid.localization.translate.lang.Languages;
+import com.airsaid.localization.translate.util.GsonUtil;
+import com.airsaid.localization.translate.util.UrlBuilder;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.Pair;
+import com.intellij.util.io.RequestBuilder;
+import icons.PluginIcons;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author musagil
+ */
+public class DeepLTranslator extends AbstractTranslator {
+
+    private static final Logger LOG = Logger.getInstance(DeepLTranslator.class);
+
+    private static final String KEY = "DeepL";
+    private static final String HOST_URL = "https://api-free.deepl.com/v2";
+    private static final String TRANSLATE_URL = HOST_URL.concat("/translate");
+    private static final String APPLY_APP_ID_URL = "https://www.deepl.com/pro-api?cta=header-pro-api/";
+
+    private List<Lang> supportedLanguages;
+
+    @Override
+    public @NotNull String getKey() {
+        return KEY;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "DeepL";
+    }
+
+    @Override
+    public @Nullable Icon getIcon() {
+        return PluginIcons.DEEP_L_ICON;
+    }
+
+    @Override
+    public boolean isNeedAppId() {
+        return false;
+    }
+
+    @Override
+    public @NotNull List<Lang> getSupportedLanguages() {
+        if (supportedLanguages == null) {
+            supportedLanguages = new ArrayList<>();
+            supportedLanguages.add(Languages.BULGARIAN);
+            supportedLanguages.add(Languages.CZECH);
+            supportedLanguages.add(Languages.DANISH);
+            supportedLanguages.add(Languages.GERMAN);
+            supportedLanguages.add(Languages.GREEK);
+            supportedLanguages.add(new Lang(118, "en-gb", "English (British)", "English (British)"));
+            supportedLanguages.add(new Lang(119, "en-us", "English (American)", "English (American)"));
+            supportedLanguages.add(Languages.SPANISH);
+            supportedLanguages.add(Languages.ESTONIAN);
+            supportedLanguages.add(Languages.FINNISH);
+            supportedLanguages.add(Languages.FRENCH);
+            supportedLanguages.add(Languages.HUNGARIAN);
+            supportedLanguages.add(Languages.INDONESIAN);
+            supportedLanguages.add(Languages.ITALIAN);
+            supportedLanguages.add(Languages.JAPANESE);
+            supportedLanguages.add(Languages.LITHUANIAN);
+            supportedLanguages.add(Languages.LATVIAN);
+            supportedLanguages.add(Languages.DUTCH);
+            supportedLanguages.add(Languages.POLISH);
+            supportedLanguages.add(new Lang(120, "pt-br", "Portuguese (Brazilian)", "Portuguese (Brazilian)"));
+            supportedLanguages.add(new Lang(121, "pt-pt", "Portuguese (European)", "Portuguese (European)"));
+            supportedLanguages.add(Languages.ROMANIAN);
+            supportedLanguages.add(Languages.RUSSIAN);
+            supportedLanguages.add(Languages.SLOVAK);
+            supportedLanguages.add(Languages.SLOVENIAN);
+            supportedLanguages.add(Languages.SWEDISH);
+            supportedLanguages.add(Languages.TURKISH);
+            supportedLanguages.add(Languages.UKRAINIAN);
+            supportedLanguages.add(Languages.CHINESE_SIMPLIFIED);
+
+        }
+        return supportedLanguages;
+    }
+
+    @Override
+    public String getAppKeyDisplay() {
+        return "KEY";
+    }
+
+    @Override
+    public @Nullable String getApplyAppIdUrl() {
+        return APPLY_APP_ID_URL;
+    }
+
+    @Override
+    public @NotNull String getRequestUrl(@NotNull Lang fromLang, @NotNull Lang toLang, @NotNull String text) {
+        return new UrlBuilder(TRANSLATE_URL).build();
+    }
+
+    @Override
+    public @NotNull List<Pair<String, String>> getRequestParams(@NotNull Lang fromLang, @NotNull Lang toLang, @NotNull String text) {
+        List<Pair<String, String>> params = new ArrayList<>();
+        params.add(Pair.create("text", text));
+        params.add(Pair.create("target_lang", toLang.getCode()));
+        return params;
+    }
+
+    @Override
+    @NotNull
+    public String getRequestBody(@NotNull Lang fromLang, @NotNull Lang toLang, @NotNull String text) {
+        return "";
+    }
+
+    @Override
+    public void configureRequestBuilder(@NotNull RequestBuilder requestBuilder) {
+        requestBuilder.tuner(connection -> {
+            connection.setRequestProperty("Authorization", "DeepL-Auth-Key " + getAppKey());
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        });
+    }
+
+    @Override
+    public @NotNull String parsingResult(@NotNull Lang fromLang, @NotNull Lang toLang, @NotNull String text, @NotNull String resultText) {
+        LOG.info("parsingResult: " + resultText);
+        return GsonUtil.getInstance().getGson().fromJson(resultText, DeepLTranslationResult.class).getTranslationResult();
+    }
+
+}
