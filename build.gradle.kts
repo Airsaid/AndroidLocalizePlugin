d32454f55c7d148691114d5fa2baf3f53ff5542e
Airsaid <airsaid1024@gmail.com>
2022-11-29 16:57:36 +0800

    Revert "Bump org.jetbrains.intellij from 1.9.0 to 1.10.0"
    This reverts commit 4ac06c32d9ba95b10d01838b34560549d2acc329.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   // Java support
   id("java")
   // Gradle IntelliJ Plugin
-  id("org.jetbrains.intellij") version "1.10.0"
+  id("org.jetbrains.intellij") version "1.9.0"
   // Gradle Changelog Plugin
   id("org.jetbrains.changelog") version "2.0.0"
 }
