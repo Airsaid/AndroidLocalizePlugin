74687019fc7278d58587a25872d2816aee173fff
Remy Benza <47558082+rbenza@users.noreply.github.com>
2023-03-14 18:56:42 +0100

    Start with adding OpenAI ChatGPT support - 4

--- src/main/java/com/airsaid/localization/translate/openai_chatgpt/ChatGPTTranslator.java
+++ src/main/java/com/airsaid/localization/translate/openai_chatgpt/ChatGPTTranslator.java
@@ -1,20 +1,3 @@
-/*
- * Copyright 2021 Airsaid. https://github.com/airsaid
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.airsaid.localization.translate.openai_chatgpt;
 
 import com.airsaid.localization.translate.AbstractTranslator;
@@ -48,7 +31,7 @@ public class ChatGPTTranslator extends AbstractTranslator {
 
     @Override
     public @NotNull String getName() {
-        return "ChatGPT";
+        return "OpenAI ChatGPT";
     }
 
     @Override
@@ -59,6 +42,11 @@ public class ChatGPTTranslator extends AbstractTranslator {
     @Override
     public boolean isNeedAppId() {
         return false;
+    }
+
+    @Override
+    public boolean isNeedAppKey() {
+        return true;
     }
 
     @Override
