7702ad2b3bbce82022724df128e7dc7357b74987
Airsaid <airsaid1024@gmail.com>
2023-03-16 11:02:08 +0800

    Rename icon and move to the orresponding package

--- src/main/java/com/airsaid/localization/translate/impl/openai/ChatGPTTranslator.java
+++ src/main/java/com/airsaid/localization/translate/impl/openai/ChatGPTTranslator.java
@@ -1,4 +1,21 @@
-package com.airsaid.localization.translate.openai_chatgpt;
+/*
+ * Copyright 2021 Airsaid. https://github.com/airsaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.airsaid.localization.translate.impl.openai;
 
 import com.airsaid.localization.translate.AbstractTranslator;
 import com.airsaid.localization.translate.lang.Lang;
@@ -7,15 +24,12 @@ import com.airsaid.localization.translate.util.GsonUtil;
 import com.google.auto.service.AutoService;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.io.RequestBuilder;
-
+import icons.PluginIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.util.List;
-
-import javax.swing.Icon;
-
-import icons.PluginIcons;
 
 
 @AutoService(AbstractTranslator.class)
@@ -36,7 +50,7 @@ public class ChatGPTTranslator extends AbstractTranslator {
 
     @Override
     public @Nullable Icon getIcon() {
-        return PluginIcons.OPENAI_CHATGPT;
+        return PluginIcons.OPENAI_ICON;
     }
 
     @Override
---
+++ src/main/java/com/airsaid/localization/translate/impl/openai/OpenAIRequest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 Airsaid. https://github.com/airsaid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.airsaid.localization.translate.impl.openai;
+
+import java.util.List;
+
+public class OpenAIRequest {
+    private String model;
+    private List<ChatGPTMessage> messages;
+
+    public OpenAIRequest(String model, List<ChatGPTMessage> messages) {
+        this.model = model;
+        this.messages = messages;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<ChatGPTMessage> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<ChatGPTMessage> messages) {
+        this.messages = messages;
+    }
+}
